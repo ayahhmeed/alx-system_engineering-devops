decf86feaab64725693766e0548506693d1f0092
ayahhmeed <ayahhmeed@gmail.com>
2024-01-30 16:27:28 -0800

    simply match

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+require 'oniguruma'
+
+def match_school(input)
+  Oniguruma::Regexp.new("School").match(input)
+end
+
+if ARGV.empty?
+  puts "Please provide an argument"
+else
+  result = match_school(ARGV[0])
+  if result
+    puts "Match found: #{result.begin(0)} - #{result.end(0)}"
+  else
+    puts "No match found"
+  end
+end
